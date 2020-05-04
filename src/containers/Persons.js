26b92b89e146ed4c16f4e68030c05378d49be548
Sibusiso Matsini <Sibusiso.Matsini@altron.com>
2020-05-04 22:59:57 +0200

    completed assignment

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,35 +1,28 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import * as actionTypes from '../store/actions';
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
-
     personAddedHandler = () => {
         const newPerson = {
             id: Math.random(), // not really unique but good enough here!
             name: 'Max',
             age: Math.floor( Math.random() * 40 )
         }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
+        this.props.onAddPerson(newPerson);
     }
 
     personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
+        this.props.onDeletePerson(personId);
     }
 
     render () {
         return (
             <div>
                 <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
+                {this.props.persons.map(person => (
                     <Person 
                         key={person.id}
                         name={person.name} 
@@ -41,4 +34,17 @@ class Persons extends Component {
     }
 }
 
-export default Persons;
+const mapStateToProps = (state) => {
+    return {
+        persons: state.persons
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onAddPerson: (person) => dispatch({type: actionTypes.ADD_PERSON, newPerson: person}),
+        onDeletePerson: (personId) => dispatch({type: actionTypes.DELETE_PERSON, personId: personId})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
---
+++ src/store/reducer.js
@@ -0,0 +1,24 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    persons: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_PERSON:
+            return {
+                ...state,
+                persons: state.persons.concat(action.newPerson)
+            }
+        case actionTypes.DELETE_PERSON:
+            const updatedPersons = state.persons.filter(person => person.id !== action.personId);
+            return {
+                ...state,
+                persons: updatedPersons
+            }
+    }
+    return state;
+};
+
+export default reducer;
